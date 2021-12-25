1dc48f20e6c5dd2177354570b5eb0eed3a32ab62
Lucas <lucasgunner3@gmail.com>
2021-12-25 20:38:06 -0300

    personalizando mensagens do identity

--- src/ProductManagement.API/Extensions/IdentityConfig.cs
+++ src/ProductManagement.API/Extensions/IdentityConfig.cs
@@ -33,10 +33,158 @@ namespace ProductManagement.API.Extensions
                         RequireUniqueEmail = true,
                     };
                 })
+                .AddErrorDescriber<MessagesConfig>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
             return services;
+        }
+
+        public class MessagesConfig : IdentityErrorDescriber
+        {
+            public override IdentityError DefaultError()
+            {
+                return new()
+                {
+                    Code = nameof(DefaultError),
+                    Description = "Ocorreu um erro desconhecido."
+                };
+            }
+
+            public override IdentityError ConcurrencyFailure()
+            {
+                return new()
+                {
+                    Code = nameof(ConcurrencyFailure),
+                    Description = "O objeto foi alterado."
+                };
+            }
+
+            public override IdentityError DuplicateEmail(string email)
+            {
+                return new()
+                {
+                    Code = nameof(DuplicateEmail),
+                    Description = $"O email {email} já existe."
+                };
+            }
+
+            public override IdentityError DuplicateRoleName(string role)
+            {
+                return new()
+                {
+                    Code = nameof(DuplicateRoleName),
+                    Description = $"O nível {role} já existe."
+                };
+            }
+
+            public override IdentityError DuplicateUserName(string userName)
+            {
+                return new()
+                {
+                    Code = nameof(DuplicateUserName),
+                    Description = $"O usuário {userName} já existe."
+                };
+            }
+
+            public override IdentityError InvalidEmail(string email)
+            {
+                return new()
+                {
+                    Code = nameof(InvalidEmail),
+                    Description = $"O email {email} está num formato inválido."
+                };
+            }
+
+            public override IdentityError InvalidRoleName(string role)
+            {
+                return new()
+                {
+                    Code = nameof(InvalidRoleName),
+                    Description = $"O nível {role} é inválido."
+                };
+            }
+
+            public override IdentityError InvalidToken()
+            {
+                return new()
+                {
+                    Code = nameof(InvalidToken),
+                    Description = "O token é inválido."
+                };
+            }
+
+            public override IdentityError InvalidUserName(string userName)
+            {
+                return new()
+                {
+                    Code = nameof(InvalidUserName),
+                    Description = $"O username {userName} é inválido."
+                };
+            }
+
+            public override IdentityError PasswordMismatch()
+            {
+                return new()
+                {
+                    Code = nameof(PasswordMismatch),
+                    Description = "usuário ou senha invalidos."
+                };
+            }
+
+            public override IdentityError PasswordTooShort(int length)
+            {
+                return new()
+                {
+                    Code = nameof(PasswordTooShort),
+                    Description = $"A senha deve conter {length} caracteres."
+                };
+            }
+
+            public override IdentityError PasswordRequiresUpper()
+            {
+                return new()
+                {
+                    Code = nameof(PasswordRequiresUpper),
+                    Description = "A senha deve conter um caractere em maíusculo."
+                };
+            }
+
+            public override IdentityError PasswordRequiresDigit()
+            {
+                return new()
+                {
+                    Code = nameof(PasswordRequiresDigit),
+                    Description = "A senha deve conter um número."
+                };
+            }
+
+            public override IdentityError PasswordRequiresLower()
+            {
+                return new()
+                {
+                    Code = nameof(PasswordTooShort),
+                    Description = "A senha deve conter um caractere em maíusculo."
+                };
+            }
+
+            public override IdentityError PasswordRequiresNonAlphanumeric()
+            {
+                return new()
+                {
+                    Code = nameof(PasswordRequiresNonAlphanumeric),
+                    Description = "A senha deve conter um caractere especial."
+                };
+            }
+
+            public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
+            {
+                return new()
+                {
+                    Code = nameof(PasswordRequiresNonAlphanumeric),
+                    Description = $"A senha deve conter {uniqueChars} caracteres especiais."
+                };
+            }
         }
     }
 }
